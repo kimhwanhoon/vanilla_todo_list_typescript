6ce27b26de790f0ba36e1715917faa86da341f44
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-07-30 17:15:53 +0900

    components/Main/WriteTodo/TodoInput.tsx
    useState => useRef

--- src/components/Main/WriteTodo/TodoInput.tsx
+++ src/components/Main/WriteTodo/TodoInput.tsx
@@ -1,33 +1,29 @@
-import React, { ChangeEvent, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { styled } from 'styled-components';
 import { useAppDispatch } from '../../../redux/config/configStore';
 import { T_todo, postTodo } from '../../../redux/modules/todo';
 
 const TodoInput: React.FC<{}> = (): JSX.Element => {
-  const [todoValue, setTodoValue] = useState<string>('');
+  console.log('rendered');
   const todoInputRef = useRef<HTMLInputElement>(null);
-
+  const todoVal = useRef<string>('');
   const dispatch = useAppDispatch();
-  //
-  // Input => onChange
-  const todoInputOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTodoValue(e.target.value);
-  };
 
-  // 작성하기 => input을 todoDB에 저장
+  // 작성하기
   const postClickHandler = (): void => {
-    if (!todoValue) {
+    if (!todoVal.current || todoInputRef.current === null) {
       alert('할 일을 적어주세요.');
       todoInputRef.current?.focus();
       return;
     }
-    setTodoValue('');
     // input값과 id를 저장
     const newData: T_todo = {
       id: Date.now(),
-      todo: todoValue,
+      todo: todoVal.current,
     };
     dispatch(postTodo(newData));
+    todoVal.current = '';
+    todoInputRef.current.value = '';
   };
 
   return (
@@ -35,8 +31,7 @@ const TodoInput: React.FC<{}> = (): JSX.Element => {
       <input
         type="text"
         ref={todoInputRef}
-        value={todoValue}
-        onChange={(e) => todoInputOnChange(e)}
+        onChange={(e) => (todoVal.current = e.target.value)}
       />
       <button onClick={postClickHandler}>작성하기</button>
     </StyledDiv>
